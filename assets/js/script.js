029694c296a752ce8e097986cce53e8003b250fe
joshacross <joshuaalexandercross@gmail.com>
2022-02-08 16:41:42 -0600

    updated event listener in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,139 +3,137 @@
 // inputs = choices
 
 // Declarations
-// var startQuiz = document.getElementById('startQuiz');
 var quizSection = document.getElementById('quizSection');
 var quizQuestion = document.getElementById('quizQuestion');
 var successMessage = document.getElementById('successMessage');
 var nameInput = document.getElementById('nameInput');
 var directions = document.getElementById('directions');
 var start = document.getElementsByClassName('start');
-// var quizSection = document.getElementById('quizSection').style.display ="none";
 
 hideElements = function() {
+    console.log("objects-hidden")
 choices.setAttribute("hidden", true);
 successMessage.setAttribute("hidden", true);
 };
 
 hideElements();
 
-var questions = [
-    {
-        question: "Would You Like To Own Your Own Home?",
-        choices: [
-            "Yes",
-            "No"
-        ],
-        answer: ""
-    },
-    {
-        question: "What Is Your Estimated Credit Score?",
-        choices: [
-            "Excellent",
-            "Good",
-            "Fair",
-            "Poor"
-        ],
-        answer: ""
-    },
-    {
-        question: "What symbol indicates a tag?",
-        choices: [
-            "Angled brackets (<>)",
-            "Cureved brackets ({})",
-            "Comma (,)",
-            "Exclamation Mark:  (!)"
-        ],
-        answer: "Angled brackets (<>)"
-    },
-    {
-        question: "Which of these is a genuine tag keyword",
-        choices: [
-            "Header",
-            "Bold",
-            "Body",
-            "Image"
-        ],
-        answer: "Body"
-    },
-    {
-        question: "(T/F) A CSS file can be applied to only one HTML file",
-        choices: [
-            "True",
-            "False"
-        ],
-        answer: "False"
-    },
-    {
-        question: "What is the corect tag for a line break in HTML",
-        choices: [
-            "<brk />",
-            "<line />",
-            "<bk />",
-            "<br />"
-        ],
-        answer: "<br />"
-    },
-    {
-        question: "What does CSS stand for?",
-        choices: [
-            "Computing Style Sheet",
-            "Creative Style System",
-            "Cascading Style Sheet",
-            "Creative Styling Sheet"
-        ],
-        answer: "Cascading Style Sheet"
-    },
-    {
-        question: "Where should a CSS file be referenced in a HTML file?",
-        choices: [
-            "Before any HTML code",
-            "After all HTML code",
-            "Inside the head section",
-            "Inside the body section"
-        ],
-        answer: "Inside the head section"
-    },
-    {
-        question: "What is the correct format for aligning written content to the center of the page in CSS?",
-        choices: [
-            "Text-align: center",
-            "Font-align: center",
-            "Text: align-center",
-            "Font: align-center"
-        ],
-        answer: "Text-align: center"
-    },
-    {
-        question: "What is the correct format for a Div?",
-        choices: [
-            "Div",
-            "<Div>",
-            "<Div />",
-            "<Div></Div>"
-        ],
-        answer: "<Div></Div>"
-    },
-]
-
-var currentQuestion = {};
-
-var startQuiz = function() {
+function startQuiz() {
+    console.log("quiz started");
     var currentQuestionIndex = 0;
 
 
-    showQuestion();
-    //Populate first question
+    var questions = [
+        {
+            question: "Would You Like To Own Your Own Home?",
+            choices: [
+                "Yes",
+                "No"
+            ],
+            answer: ""
+        },
+        {
+            question: "What Is Your Estimated Credit Score?",
+            choices: [
+                "Excellent",
+                "Good",
+                "Fair",
+                "Poor"
+            ],
+            answer: ""
+        },
+        {
+            question: "What symbol indicates a tag?",
+            choices: [
+                "Angled brackets (<>)",
+                "Cureved brackets ({})",
+                "Comma (,)",
+                "Exclamation Mark:  (!)"
+            ],
+            answer: "Angled brackets (<>)"
+        },
+        {
+            question: "Which of these is a genuine tag keyword",
+            choices: [
+                "Header",
+                "Bold",
+                "Body",
+                "Image"
+            ],
+            answer: "Body"
+        },
+        {
+            question: "(T/F) A CSS file can be applied to only one HTML file",
+            choices: [
+                "True",
+                "False"
+            ],
+            answer: "False"
+        },
+        {
+            question: "What is the corect tag for a line break in HTML",
+            choices: [
+                "<brk />",
+                "<line />",
+                "<bk />",
+                "<br />"
+            ],
+            answer: "<br />"
+        },
+        {
+            question: "What does CSS stand for?",
+            choices: [
+                "Computing Style Sheet",
+                "Creative Style System",
+                "Cascading Style Sheet",
+                "Creative Styling Sheet"
+            ],
+            answer: "Cascading Style Sheet"
+        },
+        {
+            question: "Where should a CSS file be referenced in a HTML file?",
+            choices: [
+                "Before any HTML code",
+                "After all HTML code",
+                "Inside the head section",
+                "Inside the body section"
+            ],
+            answer: "Inside the head section"
+        },
+        {
+            question: "What is the correct format for aligning written content to the center of the page in CSS?",
+            choices: [
+                "Text-align: center",
+                "Font-align: center",
+                "Text: align-center",
+                "Font: align-center"
+            ],
+            answer: "Text-align: center"
+        },
+        {
+            question: "What is the correct format for a Div?",
+            choices: [
+                "Div",
+                "<Div>",
+                "<Div />",
+                "<Div></Div>"
+            ],
+            answer: "<Div></Div>"
+        },
+    ]
 
-    // Function to populate questions from our question array.
-    function showQuestion() {
+    var currentQuestion = {};
+
+
 
         // Find question from array of questions
-        var currentQuestion = questions[currentQuestionIndex];
+    var currentQuestion = questions[currentQuestionIndex];
         // Fill in question and answer choices
-        var titleEl = quizQuestion;
+    var titleEl = quizQuestion;
         titleEl.textContent = currentQuestion.question;
         choices.innerHTML = "";
+
+    var clickChoiceButton = "";        
         // Run through questions
         currentQuestion.choices.forEach(function(choice, i) {
             var choiceEl = document.createElement("button");
@@ -147,7 +145,6 @@ var startQuiz = function() {
             // Append choice so it displays on page
             choices.appendChild(choiceEl);
         });
-    }
 }
 
 // Function for when the quiz is finished
@@ -156,10 +153,8 @@ function quizComplete() {
     question.setAttribute("class", "hide");
     // show end quiz container
     successMessage.setAttribute("hidden", false);
-    // users score equals time remaining
-    yourHighScore.textContent = time;
 }
 
 
 
-document.getElementById('startQuiz').addEventListener('click', startQuiz());
+document.getElementById('startQuiz').addEventListener("click", startQuiz);
